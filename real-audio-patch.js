e362a20f411b8f8fc3b48969f7934ab0bde2ec96
clasupernewproject <cla.bocchi@libero.it>
2026-09-03 13:27:55 +0200

    Add real environmental sounds to more levels

---
+++ real-audio-patch.js
@@ -0,0 +1,14 @@
+// Registrazioni reali da Wikimedia Commons. Se una registrazione non parte, il gioco mantiene il fallback sintetico.
+// Città
+LEVELS[1].items.find(x=>x.id==='train').audio='https://upload.wikimedia.org/wikipedia/commons/7/7a/WWS_Signalhorntrainhorn.ogg';
+LEVELS[1].items.find(x=>x.id==='bell').audio='https://upload.wikimedia.org/wikipedia/commons/9/90/Doorbell-cheap-dingdong.ogg';
+// Mare
+LEVELS[2].items.find(x=>x.id==='wave').audio='https://upload.wikimedia.org/wikipedia/commons/6/64/Ocean_Waves_on_a_Tropical_Beach.ogg';
+// Bosco
+LEVELS[4].items.find(x=>x.id==='frog').audio='https://upload.wikimedia.org/wikipedia/commons/9/9f/Single_Frog_Croak.oga';
+// Giungla
+LEVELS[5].items.find(x=>x.id==='rain').audio='https://upload.wikimedia.org/wikipedia/commons/3/3d/Rain.ogg';
+LEVELS[5].items.find(x=>x.id==='frog').audio='https://upload.wikimedia.org/wikipedia/commons/9/9f/Single_Frog_Croak.oga';
+// Notte
+LEVELS[6].items.find(x=>x.id==='frog').audio='https://upload.wikimedia.org/wikipedia/commons/9/9f/Single_Frog_Croak.oga';
+LEVELS[6].items.find(x=>x.id==='wolf').audio='https://upload.wikimedia.org/wikipedia/commons/8/87/Wolf_howls.ogg';
