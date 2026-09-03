b8405d1cff48ffb898f6315a4a0a7df4235c328b
clasupernewproject <cla.bocchi@libero.it>
2026-09-03 13:40:58 +0200

    Add real jaguar, deer, dolphin and cricket recordings

--- real-audio-patch.js
+++ real-audio-patch.js
@@ -4,6 +4,9 @@ LEVELS[1].items.find(x=>x.id==='train').audio='https://upload.wikimedia.org/wiki
 LEVELS[1].items.find(x=>x.id==='bell').audio='https://upload.wikimedia.org/wikipedia/commons/9/90/Doorbell-cheap-dingdong.ogg';
 // Mare
 LEVELS[2].items.find(x=>x.id==='wave').audio='https://upload.wikimedia.org/wikipedia/commons/6/64/Ocean_Waves_on_a_Tropical_Beach.ogg';
+LEVELS[2].items.find(x=>x.id==='dolphin').audio='https://commons.wikimedia.org/wiki/Special:Redirect/file/161691%20felixblume%20dolphin-screaming-underwater-in-caribbean-sea-mexico.wav';
+// Montagna
+LEVELS[3].items.find(x=>x.id==='deer').audio='https://commons.wikimedia.org/wiki/Special:Redirect/file/Male%20roe%20deer%20growl.ogg';
 // Bosco
 LEVELS[4].items.find(x=>x.id==='frog').audio='https://upload.wikimedia.org/wikipedia/commons/9/9f/Single_Frog_Croak.oga';
 LEVELS[4].items.find(x=>x.id==='owl').audio='https://upload.wikimedia.org/wikipedia/commons/e/e6/Short-eared_Owl.ogg';
@@ -12,7 +15,9 @@ LEVELS[5].items.find(x=>x.id==='rain').audio='https://upload.wikimedia.org/wikip
 LEVELS[5].items.find(x=>x.id==='frog').audio='https://upload.wikimedia.org/wikipedia/commons/9/9f/Single_Frog_Croak.oga';
 LEVELS[5].items.find(x=>x.id==='monkey').audio='https://upload.wikimedia.org/wikipedia/commons/b/b8/Howler_monkey.ogg';
 LEVELS[5].items.find(x=>x.id==='parrot').audio='https://upload.wikimedia.org/wikipedia/commons/7/7c/Talking_Parrot_%28Psittacula_krameri%29.ogg';
+LEVELS[5].items.find(x=>x.id==='jaguar').audio='https://commons.wikimedia.org/wiki/Special:Redirect/file/Jaguar%20saw.flac';
 // Notte
 LEVELS[6].items.find(x=>x.id==='frog').audio='https://upload.wikimedia.org/wikipedia/commons/9/9f/Single_Frog_Croak.oga';
 LEVELS[6].items.find(x=>x.id==='wolf').audio='https://upload.wikimedia.org/wikipedia/commons/8/87/Wolf_howls.ogg';
 LEVELS[6].items.find(x=>x.id==='owl').audio='https://upload.wikimedia.org/wikipedia/commons/e/e6/Short-eared_Owl.ogg';
+LEVELS[6].items.find(x=>x.id==='cricket').audio='https://commons.wikimedia.org/wiki/Special:Redirect/file/Atmo%20%E2%80%93%20Grillen%20mit%20V%C3%B6gel.mp3';
