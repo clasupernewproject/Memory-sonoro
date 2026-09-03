712d9b2a44ee1f993ec396008124c09cbf5cced7
clasupernewproject <cla.bocchi@libero.it>
2026-09-03 13:43:30 +0200

    Add real eagle and naughty sounds

--- real-audio-patch.js
+++ real-audio-patch.js
@@ -8,6 +8,7 @@ LEVELS[2].items.find(x=>x.id==='dolphin').audio='https://commons.wikimedia.org/w
 // Montagna
 LEVELS[3].items.find(x=>x.id==='deer').audio='https://commons.wikimedia.org/wiki/Special:Redirect/file/Male%20roe%20deer%20growl.ogg';
 LEVELS[3].items.find(x=>x.id==='marmot').audio='https://commons.wikimedia.org/wiki/Special:Redirect/file/Adult%20marmot%20whistling.ogv';
+LEVELS[3].items.find(x=>x.id==='eagle').audio='https://commons.wikimedia.org/wiki/Special:Redirect/file/Golden%20eagle.ogg';
 // Bosco
 LEVELS[4].items.find(x=>x.id==='frog').audio='https://upload.wikimedia.org/wikipedia/commons/9/9f/Single_Frog_Croak.oga';
 LEVELS[4].items.find(x=>x.id==='owl').audio='https://upload.wikimedia.org/wikipedia/commons/e/e6/Short-eared_Owl.ogg';
@@ -23,3 +24,7 @@ LEVELS[6].items.find(x=>x.id==='frog').audio='https://upload.wikimedia.org/wikip
 LEVELS[6].items.find(x=>x.id==='wolf').audio='https://upload.wikimedia.org/wikipedia/commons/8/87/Wolf_howls.ogg';
 LEVELS[6].items.find(x=>x.id==='owl').audio='https://upload.wikimedia.org/wikipedia/commons/e/e6/Short-eared_Owl.ogg';
 LEVELS[6].items.find(x=>x.id==='cricket').audio='https://commons.wikimedia.org/wiki/Special:Redirect/file/Atmo%20%E2%80%93%20Grillen%20mit%20V%C3%B6gel.mp3';
+// Suoni monelli: registrazioni umane reali
+LEVELS[7].items.find(x=>x.id==='fart').audio='https://commons.wikimedia.org/wiki/Special:Redirect/file/Fart.ogg';
+LEVELS[7].items.find(x=>x.id==='burp').audio='https://commons.wikimedia.org/wiki/Special:Redirect/file/Burp.ogg';
+LEVELS[7].items.find(x=>x.id==='hiccup').audio='https://commons.wikimedia.org/wiki/Special:Redirect/file/Hiccupsound.ogg';
